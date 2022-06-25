c5816b8a19c10bf9c7aba250e2856104c3d2d92c
sunsang.nahau <1587615765@qq.com>
2022-06-25 19:36:23 +0800

    user跨域问题

--- ISI/src/main/java/com/isi/utils/JwtAuthenticationFilter.java
+++ ISI/src/main/java/com/isi/utils/JwtAuthenticationFilter.java
@@ -40,7 +40,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private boolean isProtectedUrl(HttpServletRequest request) {
         List<String> protectedPaths = new ArrayList<String>();
-        protectedPaths.add("/user/info");
+//        protectedPaths.add("/user/info");
        /* protectedPaths.add("/ums/user/update");
         protectedPaths.add("/post/create");
         protectedPaths.add("/post/update");
